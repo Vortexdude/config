1ead986c759985971131ee0902283adf67602fc9
nitin <nitinnamdeo456@gmail.com>
2024-05-19 09:25:07 +0530

    :v: add automatic installation of the starship and python

---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/bash
+
+
+
+REPO_BASE_URL='https://github.com/vortexdude/'
+REPOS=
+NERDFONT_URL='https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip'
+REQUIRED_PYTHON=3.11
+
+install_base_packages(){
+	sudo apt update -y && \
+	sudo apt-get install git curl 
+	sudo add-apt-repository ppa:${repo}/ppa
+  	sudo apt update -y
+}
+
+install_python(){
+	sudo apt install -y python${REQUIRED_PYTHON} \
+    python${REQUIRED_PYTHON}-distutils \
+    python${REQUIRED_PYTHON}-venv
+    echo "Installing pip3"
+    python3_bin=$(which python3)
+    curl -sS https://bootstrap.pypa.io/get-pip.py | "${python3_bin}"
+}
+
+
+setup_nerdfont(){
+	curl $NERDFONT_URL -L -o /tmp/JetBrainsMono.zip 
+	unzip /tmp/JetBrainsMono.zip ~/.local/share/fonts/
+}
+
+install_startship(){
+	curl -sS https://starship.rs/install.sh | sh
+	echo 'eval "$(starship init bash)"' >> ~/.bashrc
+}
+
+
+def main(){
+	install_base_packages
+	install_python
+	setup_nerdfont
+	install_startship
+}
+
+
+main
